a674d1443d9f505dcc2f62e4061e91d52c84b25a
gianzk <47899960+gianzk@users.noreply.github.com>
2025-02-08 06:28:54 +0000

    update data

---
+++ proyecto/controller/reports.py
@@ -0,0 +1,28 @@
+from config.app import *
+import pandas as pd
+
+def GenerateReportVentas(app:App):
+    conn=app.bd.getConection()
+    query="""
+        SELECT 
+            p.pais,
+            v.product_id,
+            SUM(v.quantity) AS total_vendido
+        FROM 
+            VENTAS v
+        JOIN 
+            POSTALCODE p
+        ON 
+            v.postal_code = p.code
+        GROUP BY 
+            p.pais, v.product_id
+        ORDER BY 
+            total_vendido DESC;
+    """
+    df=pd.read_sql_query(query,conn)
+    path="/workspaces/workspacepy0125/proyecto/files/data-01.csv"
+    df.to_csv(path)
+    sendMail(app,path)
+
+def sendMail(app:App,data):
+    app.mail.send_email('from@example.com','Reporte','Reporte',data)
---
+++ proyecto/main.py
@@ -0,0 +1,8 @@
+from controller.menu import *
+from config.app import *
+from controller.function import *
+if __name__ == "__main__":
+    app=App('./datux.db')
+    menu(app)
+
+
